a8ab7bd0205c49e454bd8c985f5b0722ceedeaa3
LeonS2211 <lgse1021@gmail.com>
2023-07-06 17:44:12 -0500

    agregando api de universidad

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 
 import docentesRoutes from './src/routes/docentes.js';
 import cursosRoutes from './src/routes/cursos.js';
-
+import universidadesRoutes from './src/routes/universidades.js';
 
 let app = express();
 app.use(bodyParser.json());
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
     return res.json({ result: 'OK'});
 })
 
+app.use("/universidades", universidadesRoutes);
 app.use("/docentes", docentesRoutes);
 app.use("/cursos", cursosRoutes);
 
